94dab865aff1f79c38f184d1439dd77da9f3df7f
jevlogin <jev.login@mail.ru>
2021-05-13 18:32:19 +0300

    we need to make the wheels spin.

--- Assets/Code/View/CarView.cs
+++ Assets/Code/View/CarView.cs
@@ -7,6 +7,9 @@ namespace JevLogin
     internal sealed class CarView : MonoBehaviour
     {
         public Rigidbody2D Rigidbody2D;
+        public WheelJoint2D WheelJoint2DBack;
+        public WheelJoint2D WheelJoint2DForward;
+
 
         private void Awake()
         {
--- Assets/Code/View/FloatInputJoystick.cs
+++ Assets/Code/View/FloatInputJoystick.cs
@@ -11,10 +11,14 @@ namespace JevLogin
         [SerializeField] private Button _buttonGaz;
         [SerializeField] private Button _buttonJump;
         [SerializeField] private float _forceJump;
-
-        private bool _isGaz;
-        private SubscriptionProperty<bool> _isButtonPressedProperty;
         [SerializeField] private Rigidbody2D _carViewRigidbody;
+
+        private SubscriptionProperty<bool> _isButtonPressedProperty;
+        private bool _isGaz;
+        [SerializeField] private JointMotor2D _motor2DBack;
+        [SerializeField] private JointMotor2D _motor2DForward;
+        [SerializeField] private float _speedMotor;
+        private bool _activateSpeedMotor;
 
         public override void Init(SubscriptionProperty<float> leftMove, SubscriptionProperty<float> rightMove, float speed)
         {
@@ -23,7 +27,10 @@ namespace JevLogin
 
             if (_carViewRigidbody == null)
             {
-                _carViewRigidbody = FindObjectOfType<CarView>().Rigidbody2D; 
+                var car = FindObjectOfType<CarView>();
+                _carViewRigidbody = car.Rigidbody2D;
+                _motor2DBack = car.WheelJoint2DBack.motor;
+                _motor2DForward = car.WheelJoint2DForward.motor;
             }
 
             _buttonJump.onClick.AddListener(JumpCar);
@@ -43,7 +50,7 @@ namespace JevLogin
         {
             _carViewRigidbody.AddForce(Vector2.up * _speed * _forceJump, ForceMode2D.Impulse);
         }
-       
+
 
         private void SwitchValue(bool value)
         {
@@ -63,7 +70,32 @@ namespace JevLogin
                 {
                     OnLeftMove(moveStep);
                 }
+                if (!_activateSpeedMotor)
+                {
+                    RunMotorWheel();
+                }
             }
+            else if (_activateSpeedMotor)
+            {
+                StopMotorWheel();
+            }
+        }
+
+        private void StopMotorWheel()
+        {
+            _activateSpeedMotor = false;
+            _motor2DBack.motorSpeed = 0.0f;
+            _motor2DForward.motorSpeed = 0.0f;
+        }
+
+        private void RunMotorWheel()
+        {
+            _activateSpeedMotor = true;
+            _motor2DBack.motorSpeed = _speedMotor;
+            _motor2DForward.motorSpeed = _speedMotor;
+
+            Debug.Log($"RunMotorWheel - _motor2DBack.motorSpeed = {_motor2DBack.motorSpeed}");
+            Debug.Log($"RunMotorWheel - _motor2DBack.motorSpeed = {_motor2DForward.motorSpeed}");
         }
     }
 }
