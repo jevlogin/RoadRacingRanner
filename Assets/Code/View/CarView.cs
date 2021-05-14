0c6ac2f9b1a2dd0cab55f1235602db8294368ff5
jevlogin <jev.login@mail.ru>
2021-05-14 17:10:53 +0300

    added a brain for driving a car, and added the standard assets

--- Assets/Code/View/CarView.cs
+++ Assets/Code/View/CarView.cs
@@ -1,4 +1,6 @@
-﻿using UnityEngine;
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
 
 
 namespace JevLogin
@@ -10,22 +12,36 @@ namespace JevLogin
         public WheelJoint2D WheelJoint2DBack;
         public WheelJoint2D WheelJoint2DForward;
         public SubscriptionProperty<bool> IsRotateWheels = new SubscriptionProperty<bool>();
-        
+
         private JointMotor2D _motorStop;
         private JointMotor2D _motorActivated;
 
         [SerializeField] private float _speedMotor;
+        [SerializeField] private List<ParticleSystem> _partikles = new List<ParticleSystem>();
+        [SerializeField] private AudioClip _audioStartEngine;
+        [SerializeField] private AudioClip _audioWorkingOnIdle;
+        [SerializeField] private AudioClip _audioSoundInMotion;
+        [SerializeField] private AudioSource _audioSource;
+        [SerializeField] private float _diffAudio = 0.01f;
 
         private void Awake()
         {
             Rigidbody2D = GetComponent<Rigidbody2D>();
-
+            _audioSource = GetComponent<AudioSource>();
             _motorStop = WheelJoint2DBack.motor;
 
             _motorActivated = _motorStop;
             _motorActivated.motorSpeed = _speedMotor;
 
             IsRotateWheels.SubscriptionOnChange(SwitchValueRotate);
+            Invoke(nameof(PlayCarEngine), _audioStartEngine.length - _diffAudio);
+        }
+
+        private void PlayCarEngine()
+        {
+            _audioSource.clip = _audioWorkingOnIdle;
+            _audioSource.loop = true;
+            _audioSource.Play();
         }
 
         private void OnDestroy()
@@ -38,10 +54,30 @@ namespace JevLogin
             if (IsRotateWheels.Value)
             {
                 RunMotorWheel();
+                foreach (var particle in _partikles)
+                {
+                    particle.Play();
+                }
+                SwitchAudioClip(_audioSoundInMotion);
             }
             else
             {
                 StopMotorWheel();
+                foreach (var particle in _partikles)
+                {
+                    particle.Stop();
+                }
+                SwitchAudioClip(_audioWorkingOnIdle);
+            }
+        }
+
+        private void SwitchAudioClip(AudioClip audioSoundInMotion)
+        {
+            if (_audioSource.isPlaying)
+            {
+                _audioSource.Pause();
+                _audioSource.clip = audioSoundInMotion;
+                _audioSource.Play();
             }
         }
 
