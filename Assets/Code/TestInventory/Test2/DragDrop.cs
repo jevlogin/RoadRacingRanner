7eb95e7890c07deaed43131d630a0c4ee292d0f2
jevlogin <jev.login@mail.ru>
2021-06-02 10:15:57 +0300

    rf

---
+++ Assets/Code/TestInventory/Test2/DragDrop.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+
+namespace JevLogin
+{
+    internal sealed class DragDrop : MonoBehaviour, IPointerDownHandler
+    {
+        public void OnPointerDown(PointerEventData eventData)
+        {
+            Debug.Log($"OnPointerDown");
+        }
+    }
+}
